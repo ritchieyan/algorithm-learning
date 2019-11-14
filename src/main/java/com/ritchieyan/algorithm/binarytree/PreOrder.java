0628581265c19a587aa8411bc6e1d088dcceedbf
bianlifeng <ziqi.yan@bianlifeng.com>
2019-11-14 21:00:14 +0800

    init

---
+++ src/main/java/com/ritchieyan/algorithm/binarytree/PreOrder.java
@@ -0,0 +1,10 @@
+package com.ritchieyan.algorithm.binarytree;
+
+/**
+ * TODO completion javadoc.
+ *
+ * @author Ritchie.Yan
+ * @since 14 十一月 2019
+ */
+public class PreOrder {
+}
---
+++ src/main/java/com/ritchieyan/algorithm/sort/QuickSort.java
@@ -0,0 +1,10 @@
+package com.ritchieyan.algorithm.sort;
+
+/**
+ * 快速排序
+ *
+ * @author Ritchie.Yan
+ * @since 14 十一月 2019
+ */
+public class QuickSort {
+}
